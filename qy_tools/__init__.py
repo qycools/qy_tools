c71d6bf0400a637516fe2ecc154706b8fb3efb64
Qiao Yang <79189465+qycools@users.noreply.github.com>
2025-07-07 11:26:45 +0800

    Update __init__.py

--- qy_tools/__init__.py
+++ qy_tools/__init__.py
@@ -3,4 +3,5 @@
 __version__ = "0.1.0"
 from .multiprocess import multipool,multipool_path_args
 from .image_process import combine_images, array2img,crop_img,npy_to_rgb
-__all__ = ['multipool',  'combine_images', 'array2img','multipool_path_args','crop_img',"npy_to_rgb"]
+from .image_cal import compute_rgb_stats_gpu,compute_rgb_stats
+__all__ = ['multipool',  'combine_images', 'array2img','multipool_path_args','crop_img',"npy_to_rgb","compute_rgb_stats_gpu","compute_rgb_stats"]
