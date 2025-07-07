ae953e393946b4d9db46bc6dd030df5b2ac1485c
Qiao Yang <79189465+qycools@users.noreply.github.com>
2025-07-07 10:47:53 +0800

    Update __init__.py

--- qy_tools/__init__.py
+++ qy_tools/__init__.py
@@ -2,5 +2,5 @@
 
 __version__ = "0.1.0"
 from .multiprocess import multipool,multipool_path_args
-from .image_process import combine_images, array2img,crop_img
-__all__ = ['multipool',  'combine_images', 'array2img','multipool_path_args','crop_img']
+from .image_process import combine_images, array2img,crop_img,npy_to_rgb
+__all__ = ['multipool',  'combine_images', 'array2img','multipool_path_args','crop_img',"npy_to_rgb"]
